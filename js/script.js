80e00fdbf06c4bf44d866a4cf80698f7ce529147
gustavomarim <gustavo.dantas.marim@outlook.com>
2022-04-05 12:02:50 -0300

    Accordion List

--- js/script.js
+++ js/script.js
@@ -1,24 +1,48 @@
-// Criando Navegação por Tabs = Associando imagem a descrição => Relação 1x1
-const tabMenu = document.querySelectorAll('.js-tabmenu li');
-const tabContent = document.querySelectorAll('.js-tabcontent section');
+function initTabNav() {
+    // Criando Navegação por Tabs = Associando imagem a descrição => Relação 1x1
+    const tabMenu = document.querySelectorAll('.js-tabmenu li');
+    const tabContent = document.querySelectorAll('.js-tabcontent section');
 
-if (tabMenu.length && tabContent.length) {
-    // Primeiro Item Animais ativo
-    tabContent[0].classList.add('ativo');
+    if (tabMenu.length && tabContent.length) {
+        // Primeiro Item Animais ativo
+        tabContent[0].classList.add('ativo');
 
-    function activeTab(index) {
-        tabContent.forEach((section) => {
-            section.classList.remove('ativo');
+        function activeTab(index) {
+            tabContent.forEach((section) => {
+                section.classList.remove('ativo');
+            });
+
+            tabContent[index].classList.add('ativo');
+        }
+
+        tabMenu.forEach((itemMenu, index) => {
+            itemMenu.addEventListener('click', () => {
+                activeTab(index);
+            });
         });
-
-        tabContent[index].classList.add('ativo');
     }
-
-    tabMenu.forEach((itemMenu, index) => {
-        itemMenu.addEventListener('click', () => {
-            activeTab(index);
-        });
-    });
 }
 
+initTabNav();
 
+function initAccordion() {
+    // Criando Acordion List
+    const accordionList = document.querySelectorAll('.js-accordion dt');
+    const activeClass = 'ativo';
+
+    if (accordionList.length) {
+        accordionList[0].classList.add(activeClass); // Primeiro item ativo DT
+        accordionList[0].nextElementSibling.classList.add(activeClass); // Primeiro item ativo DD
+
+        function activeAccordion(event) {
+            this.classList.toggle(activeClass);
+            this.nextElementSibling.classList.toggle(activeClass);
+        }
+
+        accordionList.forEach((item) => {
+            item.addEventListener('click', activeAccordion);
+        });
+    }
+}
+
+initAccordion();
