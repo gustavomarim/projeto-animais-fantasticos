a71e320c8ac9694ffe1da9ad61cfa17d44aa68cf
gustavomarim <gustavo.dantas.marim@outlook.com>
2022-04-05 12:37:54 -0300

    Animação ao Scroll

--- js/script.js
+++ js/script.js
@@ -46,3 +46,59 @@ function initAccordion() {
 }
 
 initAccordion();
+
+function initSmoothScroll() {
+    const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+
+    function scrollToSection(event) {
+        event.preventDefault();
+        const href = event.currentTarget.getAttribute('href');
+        const section = document.querySelector(href);
+        // const windowTop = section.offsetTop;
+
+        // Scroll suave para a seção
+        // window.scrollTo({
+        //     top: windowTop,
+        //     behavior: 'smooth',
+        // });
+
+        // Forma alternativa para o scroll suave
+        section.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start',
+        });
+    }
+
+    linksInternos.forEach((link) => {
+        link.addEventListener('click', scrollToSection);
+    });
+}
+
+initSmoothScroll();
+
+function initScrollAnimation() {
+    const sections = document.querySelectorAll('.js-scroll');
+
+    if (sections.length) {
+
+        const halfHeightWindow = window.innerHeight * 0.6;
+
+        function scrollAnimation() {
+            sections.forEach((section) => {
+                const sectionTop = section.getBoundingClientRect().top;
+                const isSectionVisible = (sectionTop - halfHeightWindow) < 0;
+                if (isSectionVisible) {
+                    section.classList.add('ativo');
+                } else {
+                    section.classList.remove('ativo');
+                }
+            });
+        }
+
+        scrollAnimation();
+
+        window.addEventListener('scroll', scrollAnimation);
+    }
+}
+
+initScrollAnimation();
