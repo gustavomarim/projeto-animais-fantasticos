814ce9d460750561c37f038e65644abda39a066b
unknown <gustavo.dantas.marim@outlook.com>
2022-04-24 12:52:31 -0300

    Adicionando Tooltip ao mapa

--- js/script.js
+++ js/script.js
@@ -3,9 +3,11 @@ import { initModal } from "./modules/modal.js";
 import { initAnimacaoScroll } from "./modules/scroll-animacao.js";
 import { initScrollSuave } from "./modules/scroll.suave.js";
 import { initTabNav } from "./modules/tab-nav.js";
+import { initToolTip } from "./modules/tooltip.js";
 
 initTabNav();
 initAccordion();
 initScrollSuave();
 initAnimacaoScroll();
 initModal();
+initToolTip();
