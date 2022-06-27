0834c08ad5940e82439fbbcc73f92ff8d73a33d5
unknown <gustavo.dantas.marim@outlook.com>
2022-06-27 17:06:19 -0300

    fix: Correções gerais de bugs Corrigindo os bugs gerais de estrutura, estilo e lógica do site.

--- js/modules/menu-mobile.js
+++ js/modules/menu-mobile.js
@@ -14,7 +14,8 @@ export default class MenuMobile {
     this.openMenu = this.openMenu.bind(this);
   }
 
-  openMenu() {
+  openMenu(event) {
+    event.preventDefault(); // fix: previne efeito de click no mobile
     this.menuList.classList.add(this.activeClass);
     this.menuButton.classList.add(this.activeClass);
     outsideClick(this.menuList, this.events, () => {
